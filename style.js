d1cb52ecf407549030d162a000810effce3c118e
gj2121 <47671830+gj2121@users.noreply.github.com>
2021-03-13 16:50:54 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { blue, pink } from '@material-ui/core/colors';
+
+export default makeStyles((theme) => ({
+  avatarIncome: {
+    color: '#fff',
+    backgroundColor: blue[500],
+  },
+  avatarExpense: {
+    color: theme.palette.getContrastText(pink[500]),
+    backgroundColor: pink[500],
+  },
+  list: {
+    
+    maxHeight: '150px',
+    overflow: 'auto',
+  },
+}));
