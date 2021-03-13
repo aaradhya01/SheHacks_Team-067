a3c37c8839bb12656f496c2ad74834877791562c
Aaradhya <aaradhyabansal01@gmail.com>
2021-03-13 09:37:26 +0530

    making cards

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
+
+const Details = () => {
+  return (
+    <Card className={title === "Income" ? classes.income : classes.expense}>
+      <CardHeader title={title} />
+      <CardContent>
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Details;
